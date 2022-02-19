9a2d61841325439ea214dd66ca363cfb678ecd36
Shardulkumbhar <kumbharshardul384@gmail.com>
2022-02-19 08:52:20 +0530

    added uc1 problem

---
+++ UC1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class UC1 {
+
+	public static void main(String[] args) {
+		
+		// taking input
+		Scanner scanner =new Scanner(System.in);
+        System.out.println("Enter the Stake Day:");
+     	int s =scanner.nextInt();
+     	System.out.println("how much to bet:");
+      	int b =scanner.nextInt();
+     	
+		}
+
+	}
+
